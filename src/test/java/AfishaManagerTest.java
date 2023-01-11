700053db37c793472fc58bee3a2a212da68ab908
Soboleva Ekaterina <soboleva.ketti0403@gmail.com>
2023-01-12 01:05:14 +0300

    1

---
+++ src/test/java/AfishaManagerTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class AfishaManagerTest {
+
+    @Test
+    public void test() {
+        AfishaManager afisha = new AfishaManager(3);
+        afisha.add("Movie 1");
+        afisha.add("Movie 2");
+        afisha.add("Movie 3");
+        afisha.add("Movie 4");
+        afisha.add("Movie 5");
+
+        String[] expected = {"Movie 5", "Movie 4", "Movie 3"};
+        String[] actual = afisha.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
