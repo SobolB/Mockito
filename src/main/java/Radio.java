3212c6c6e745b9c1f310444a475211f881684e64
SobolB <79850869+SobolB@users.noreply.github.com>
2023-05-16 14:18:51 +0300

    Update Radio.java

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -3,29 +3,32 @@ package ru.netology.statistic;
 public class Radio {
     private int currentStation;
     private int currentVolume;
-    private int maxStation;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+    private int maxRadioStation;
+    private int minRadioStation = 0;
 
     public Radio() {
-        maxStation = 9;
+        maxRadioStation = 9;
     }
 
     public Radio(int stationsCount) {
-        maxStation = stationsCount - 1;
+        maxRadioStation = stationsCount - 1;
     }
 
     public void next() {
-        if (currentStation < maxStation) {
-            currentStation++;
-        } else {
-            currentStation = 0;
+        if (currentStation != maxRadioStation) {
+            currentStation = currentStation + 1;
+            return;
         }
+        currentStation = minRadioStation;
     }
 
     public void prev() {
-        if (currentStation > 0) {
-            currentStation--;
+        if (currentStation != minRadioStation) {
+            currentStation = currentStation - 1;
         } else {
-            currentStation = maxStation;
+            currentStation = maxRadioStation;
         }
     }
 
@@ -34,26 +37,43 @@ public class Radio {
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation < 0) {
+        if (currentStation < minRadioStation) {
             return;
         }
-        if (currentStation > maxStation) {
+        if (currentStation > maxRadioStation) {
             return;
         }
         this.currentStation = currentStation;
     }
+
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
+        if (currentVolume < minVolume) {
             return;
         }
-        if (currentVolume > 100) {
+        if (currentVolume > maxVolume) {
             return;
         }
         this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume = currentVolume + 1;
+        } else {
+            currentVolume = maxVolume;
+        }
+    }
+
+    public void reduceVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        } else {
+            currentVolume = minVolume;
+        }
     }
 }
