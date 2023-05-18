6965e6d16b83862809b537e87af551e41f560c23
SobolB <79850869+SobolB@users.noreply.github.com>
2023-05-18 14:27:58 +0300

    Update FilmManager.java

--- src/main/java/FilmManager.java
+++ src/main/java/FilmManager.java
@@ -4,15 +4,15 @@ public class FilmManager {
     private String[] films = new String[0];
     private int limit;
 
-    public FilmManager(){
+    public FilmManager() {
         this.limit = 10;
     }
 
-    public FilmManager(int limit){
+    public FilmManager(int limit) {
         this.limit = limit;
     }
 
-    public void addFilm(String film){
+    public void addFilm(String film) {
         String[] tmp = new String[films.length + 1];
         for (int i = 0; i < films.length; i++) {
             tmp[i] = films[i];
@@ -21,20 +21,20 @@ public class FilmManager {
         films = tmp;
     }
 
-    public String[] findAll(){
+    public String[] findAll() {
         return films;
     }
 
     public String[] findLast() {
         int resultLength;
-        if (films.length < 10) {
+        if (films.length < this.limit) {
             resultLength = films.length;
-        }else{
-            resultLength = 10;
+        } else {
+            resultLength = this.limit;
         }
         String[] tmp = new String[resultLength];
         for (int i = 0; i < tmp.length; i++) {
-            tmp[i] = films[films.length -1-i];
+            tmp[i] = films[films.length - 1 - i];
         }
         return tmp;
     }
